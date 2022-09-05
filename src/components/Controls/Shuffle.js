127c7dc5c8340e012c08e7a40f0be58b6f49fe2e
minaibrahimsaid <minaibrahimsaid@gmail.com>
2022-09-05 12:01:58 -0400

    add shuffle function

--- src/components/Controls/Shuffle.js
+++ src/components/Controls/Shuffle.js
@@ -1,8 +1,11 @@
-import { BsShuffle  } from 'react-icons/bs'
+import { BsShuffle } from 'react-icons/bs'
+import { shuffle } from 'lodash'
 
-const Shuffle = ({ shuffle = () => {} }) => {
+const Shuffle = ({setCookie ,cards =[]}) => {
   const onShuffle = () => {
-    shuffle()
+    const allCard = cards ?? []
+    const newCookie = JSON.stringify([...shuffle(allCard)])
+    setCookie('cards', newCookie)
   }
 
   return <BsShuffle className="icon" size={25} onClick={onShuffle} />
--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,12 +1,14 @@
 import Add from './Add'
+import Shuffle from './Shuffle'
 
 const Controls = ({
-  setCookie = () => {},
+  setCookie = () => { },
   cards = [],
 }) => {
   return (
     <div className="controls">
       <Add setCookie={setCookie} cards={cards} />
+      <Shuffle setCookie={setCookie} cards={cards} />
     </div>
   )
 }
